6c93b907d10cf17ab27d8f9b7196abf5aad7a820
Nick <ngstafford1@gmail.com>
2024-10-06 09:11:47 -0400

    added themery

---
+++ unused/themery.lua
@@ -0,0 +1,12 @@
+return {
+
+	"zaldih/themery.nvim",
+	lazy = false,
+	config = function()
+		require("themery").setup({
+			-- add the config here
+			themes = { "catppuccin", "solarized-osaka", "gruvbox", "tokyonight", "habamax" }, -- Your list of installed colorschemes.
+			livePreview = true, -- Apply theme while picking. Default to true.
+		})
+	end,
+}

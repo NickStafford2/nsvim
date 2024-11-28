922ee7996bd19df6f695fb3fd9bc28d7b7da779c
Nicholas Stafford <ngstafford1@gmail.com>
2024-11-27 23:03:09 -0500

    added dial

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -51,6 +51,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.util.mini-hipatterns" },
 		-- { import = "lazyvim.plugins.extras.test.core" },
 		{ import = "lazyvim.plugins.extras.coding.yanky" },
+		{ import = "lazyvim.plugins.extras.editor.dial" },
 		-- { import = "lazyvim.plugins.extras.editor.mini-files" },
 		-- { import = "lazyvim.plugins.extras.util.project" },
 		{ import = "plugins" },

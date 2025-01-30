824812aa661aa793cb68234966c189f3eb2f23e5
Nicholas Stafford <ngstafford1@gmail.com>
2025-01-30 14:51:15 -0500

    removed duplicate

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -24,12 +24,4 @@ return {
 			position = "right",
 		},
 	},
-
-	{
-		"nvim-cmp",
-		dependencies = { "hrsh7th/cmp-emoji" },
-		opts = function(_, opts)
-			table.insert(opts.sources, { name = "emoji" })
-		end,
-	},
 }

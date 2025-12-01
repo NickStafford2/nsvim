52ce830eae1f9193b1f35130d8db4075fbf23dbe
Nicholas Stafford <ngstafford1@gmail.com>
2025-12-01 03:56:49 -0500

    added oil back

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,15 +1,13 @@
 return {
 	"stevearc/oil.nvim",
 	opts = {
-		default_file_explorer = true, -- replaces netrw
+		default_file_explorer = true,
 		skip_confirm_for_simple_edits = true,
-		view_options = {
-			show_hidden = true,
-		},
+		view_options = { show_hidden = true },
 	},
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	keys = {
 		{ "-", "<cmd>Oil<CR>", desc = "Open parent directory" },
-		{ "<leader>e", "<cmd>Oil<CR>", desc = "Oil Explorer" },
+		{ "<leader>e", false }, -- disable Oil stealing <leader>e
 	},
 }

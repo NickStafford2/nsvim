1f25bf3bf66f3aae46a39f5c00b86269434526e2
Nicholas Stafford <ngstafford1@gmail.com>
2025-11-30 20:39:28 -0500

    simplified init.lua

--- init.lua
+++ init.lua
@@ -2,12 +2,14 @@ if vim.loader then
 	vim.loader.enable()
 end
 
+-- Debug function
 _G.dd = function(...)
 	require("util.debug").dump(...)
 end
 vim.print = _G.dd
 
+-- Core LazyVim + plugin loader
 require("config.lazy")
-require("polish")
-require("lspconfig").glsl_analyzer.setup({})
--- require("lspconfig").clangd.setup({})
+
+-- Optional: final custom code (keep VERY minimal)
+-- require("lspconfig").glsl_analyzer.setup({})

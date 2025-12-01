5f0b8c6dedb69f8c515abc8f40cc1d126c4c2e38
Nicholas Stafford <ngstafford1@gmail.com>
2025-11-30 19:43:47 -0500

    fixed colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,12 @@
-if true then
-	return {}
-end
-
 return {
-	"personal/solarized-osaka.nvim",
-	lazy = true,
-	priority = 1000,
-	opts = function()
-		return {
-			transparent = true,
-		}
-	end,
+  "craftzdog/solarized-osaka.nvim",
+  lazy = false,          -- load early so colors & modules are available
+  priority = 1000,
+  opts = {
+    transparent = true,
+  },
+  config = function(_, opts)
+    require("solarized-osaka").setup(opts)
+    -- LazyVim will run :colorscheme solarized-osaka for us
+  end,
 }

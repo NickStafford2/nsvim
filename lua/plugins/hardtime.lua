6126eec654df28bf9fed2e8348ba8d779bd46001
Nicholas Stafford <ngstafford1@gmail.com>
2025-11-30 21:28:32 -0500

    changed extras. removed some for now

--- lua/plugins/hardtime.lua
+++ lua/plugins/hardtime.lua
@@ -1,6 +1,6 @@
--- if true then
--- 	return {}
--- end
+if true then
+	return {}
+end
 
 return {
 	"m4xshen/hardtime.nvim",
--- lua/plugins/inc-rename.lua
+++
@@ -1,7 +0,0 @@
--- return { "saecki/live-rename.nvim" }
-return {
-	"smjonas/inc-rename.nvim",
-	config = function()
-		require("inc_rename").setup()
-	end,
-}

a94ecc52e8854677f1f8ea581634c610f000d7c9
Nicholas Stafford <ngstafford1@gmail.com>
2025-11-30 23:21:30 -0500

    added a thing to expland which-key

---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,20 @@
+if true then
+	return {}
+end
+return {
+	"folke/which-key.nvim",
+	opts = {
+		-- Sometimes which-key misses some keymaps. add them here
+		-- Add missing <leader>s group entries
+		defaults = {
+			["<leader>s"] = { name = "+search" },
+
+			["<leader>sn"] = { "<cmd>Noice history<cr>", "Noice History" },
+			["<leader>ss"] = { "<cmd>lua require('telescope.builtin').lsp_document_symbols()<cr>", "Document Symbols" },
+			["<leader>sS"] = {
+				"<cmd>lua require('telescope.builtin').lsp_workspace_symbols()<cr>",
+				"Workspace Symbols",
+			},
+		},
+	},
+}

2732ea28a745646591960af87c7fedc7e73f10ca
= <ngstafford1@gmail.com>
2025-12-01 16:56:01 -0500

    removed keymaps i don't use. now there are no keymap conflicts.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -16,18 +16,18 @@ local opts = { noremap = true, silent = true }
 -- end, { desc = "Neo-tree + Harpoon sidebar" })
 
 -- Do things without affecting the registers
-keymap.set("n", "x", '"_x')
-keymap.set("n", "<Leader>p", '"0p')
-keymap.set("n", "<Leader>P", '"0P')
-keymap.set("v", "<Leader>p", '"0p')
-keymap.set("n", "<Leader>c", '"_c')
-keymap.set("n", "<Leader>C", '"_C')
-keymap.set("v", "<Leader>c", '"_c')
-keymap.set("v", "<Leader>C", '"_C')
-keymap.set("n", "<Leader>d", '"_d')
-keymap.set("n", "<Leader>D", '"_D')
-keymap.set("v", "<Leader>d", '"_d')
-keymap.set("v", "<Leader>D", '"_D')
+-- keymap.set("n", "x", '"_x')
+-- keymap.set("n", "<Leader>p", '"0p')
+-- keymap.set("n", "<Leader>P", '"0P')
+-- keymap.set("v", "<Leader>p", '"0p')
+-- keymap.set("n", "<Leader>c", '"_c')
+-- keymap.set("n", "<Leader>C", '"_C')
+-- keymap.set("v", "<Leader>c", '"_c')
+-- keymap.set("v", "<Leader>C", '"_C')
+-- keymap.set("n", "<Leader>d", '"_d')
+-- keymap.set("n", "<Leader>D", '"_D')
+-- keymap.set("v", "<Leader>d", '"_d')
+-- keymap.set("v", "<Leader>D", '"_D')
 
 -- Delete a word backwards
 -- keymap.set("n", "dw", 'vb"_d') -- why would anyone do this?
@@ -47,24 +47,24 @@ keymap.set("n", "<Leader>O", "O<Esc>^Da", opts)
 keymap.set("n", "<C-m>", "<C-i>", opts)
 
 -- New tab
-keymap.set("n", "te", ":tabedit")
+-- keymap.set("n", "te", ":tabedit")
 -- keymap.set("n", "<tab>", ":tabnext<Return>", opts)
 -- keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
 
 -- Split window
-keymap.set("n", "ss", ":split<Return>", opts)
-keymap.set("n", "sv", ":vsplit<Return>", opts)
+-- keymap.set("n", "ss", ":split<Return>", opts)
+-- keymap.set("n", "sv", ":vsplit<Return>", opts)
 -- Move window
-keymap.set("n", "sh", "<C-w>h")
-keymap.set("n", "sk", "<C-w>k")
-keymap.set("n", "sj", "<C-w>j")
-keymap.set("n", "sl", "<C-w>l")
+-- keymap.set("n", "sh", "<C-w>h")
+-- keymap.set("n", "sk", "<C-w>k")
+-- keymap.set("n", "sj", "<C-w>j")
+-- keymap.set("n", "sl", "<C-w>l")
 
 -- Resize window
-keymap.set("n", "<C-w><left>", "<C-w><")
-keymap.set("n", "<C-w><right>", "<C-w>>")
-keymap.set("n", "<C-w><up>", "<C-w>+")
-keymap.set("n", "<C-w><down>", "<C-w>-")
+-- keymap.set("n", "<C-w><left>", "<C-w><")
+-- keymap.set("n", "<C-w><right>", "<C-w>>")
+-- keymap.set("n", "<C-w><up>", "<C-w>+")
+-- keymap.set("n", "<C-w><down>", "<C-w>-")
 
 -- keymap.set("n", "<leader>r", function()
 -- 	require("personal.hsl").replaceHexWithHSL()

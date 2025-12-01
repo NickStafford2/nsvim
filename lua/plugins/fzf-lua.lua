943b5ed3bac3fb0f98ef92fbd1c13d4538b936b2
Nicholas Stafford <ngstafford1@gmail.com>
2025-11-30 20:27:36 -0500

    fixed treesitter and fzf-lua. installed new fzf lua

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -1,6 +1,17 @@
 return {
 	"ibhagwan/fzf-lua",
+	cmd = "FzfLua",
 	opts = {
+		fzf_bin = "sk", -- use Rust skim instead of fzf
 		file_ignore_patterns = { "names.json" },
+		winopts = {
+			preview = { vertical = "down:40%" },
+		},
+	},
+	keys = {
+		{ "<leader>ff", "<cmd>FzfLua files<cr>", desc = "Find Files" },
+		{ "<leader>fg", "<cmd>FzfLua live_grep<cr>", desc = "Live Grep" },
+		{ "<leader>fw", "<cmd>FzfLua grep_cword<cr>", desc = "Grep Word Under Cursor" },
+		{ "<leader>fb", "<cmd>FzfLua buffers<cr>", desc = "Buffers" },
 	},
 }
